4fbe757e7049064882f7942ee404af16cd3e1d97
rowen101 <rgrowengonzales66@gmail.com>
2021-12-03 12:39:54 +0800

    update add new ticket

---
+++ pages/new.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import { useState, useEff } from "react";
+
+const NewTicket = () => {
+  return (
+    <>
+      <p>New Ticket</p>
+    </>
+  );
+};
+
+export default NewTicket;
